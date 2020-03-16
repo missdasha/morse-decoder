1167daa39407a63273a4b31f04615628e5f14708
missukevitch <missukevitch@gmail.com>
2020-03-16 18:47:28 +0300

    feat: resolve task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,30 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let i = 0;
+    let sequence = "";
+    let str = "";
+    let res = "";
+    let k;
+    while(i <= expr.length-10) {
+        sequence = expr.slice(i, i+10);
+        k = 0;
+        while(k <= 8) {
+            if(sequence.substring(k, k+2) === "10")
+                str += '.';
+            else if(sequence.substring(k, k+2) === "11")
+                    str += '-';  
+            else if(sequence.substring(k, k+2) === "**") {
+                    str +=' ';
+                    break;
+                }
+            k+=2;  
+        }
+        str !== " " ? res += MORSE_TABLE[str] : res += " ";
+        i+=10;
+        str = "";
+    }
+    return res;
 }
 
 module.exports = {
